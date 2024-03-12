92542f4d1f6a8bc1da63752d07aad21dc4cc6f24
sbx_user1051 <sbx_user1051@169.254.4.241>
2024-03-12 14:53:57 +0000

    Tue Mar 12 14:53:57 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,4 +3,10 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    if not isinstance(skus, str):
+        return -1
+
+    item_counts = {}
+
+def count_items(skus, item_counts):
+    for 
