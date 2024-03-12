09beb6703ee2b4ab550495afc10cc31a920606db
sbx_user1051 <sbx_user1051@169.254.4.241>
2024-03-12 15:20:57 +0000

    Tue Mar 12 15:20:57 UTC 2024

--- test/solution_tests/CHK/test_check.py
+++ test/solution_tests/CHK/test_check.py
@@ -4,3 +4,7 @@ from lib.solutions.CHK import checkout_solution
 class TestCheck():
     def test_check(self):
         assert checkout_solution.checkout('AAA') == 130
+        assert checkout_solution.checkout('BB') == 45
+        assert checkout_solution.checkout('AAABB') == 175
+        assert checkout_solution.checkout('AAAA') == 180
+
