aa721717cebb53c4391586d0409db3446045617c
sbx_user1051 <sbx_user1051@169.254.4.241>
2024-03-12 14:56:57 +0000

    Tue Mar 12 14:56:57 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -7,6 +7,18 @@ def checkout(skus):
         return -1
 
     item_counts = {}
+    is_counted = count_items(skus, item_counts)
+    if is_counted == -1:
+        return -1
 
 def count_items(skus, item_counts):
-    for 
+    for item in skus:
+        if item not in item_counts:
+            return -1
+
+        if item in item_counts:
+            item_counts[item] += 1
+        else:
+            item_counts[item] = 1
+
+        return 0
