c9b17f66ac724ea8b73594c3aec1f850be978669
sbx_user1051 <sbx_user1051@169.254.17.197>
2024-03-12 22:23:29 +0000

    Tue Mar 12 22:23:29 UTC 2024

--- test/solution_tests/CHK/test_check.py
+++ test/solution_tests/CHK/test_check.py
@@ -2,75 +2,76 @@ from lib.solutions.CHK import checkout_solution
 
 
 class TestCheck():
-    # def test_1(self):
-    #     assert checkout_solution.checkout('AAA') == 130
-    #
-    # def test_2(self):
-    #     assert checkout_solution.checkout('BB') == 45
-    #
-    # def test_3(self):
-    #     assert checkout_solution.checkout('AAABB') == 175
-    #
-    # def test_4(self):
-    #     assert checkout_solution.checkout('AAAA') == 180
-    #
-    # def test_5(self):
-    #     assert checkout_solution.checkout('BBB') == 75
-    #
-    # def test_6(self):
-    #     assert checkout_solution.checkout('AAAAABBB') == 275
-    #
-    # def test_7(self):
-    #     assert checkout_solution.checkout('ABCD') == 115
-    #
-    # def test_8(self):
-    #     assert checkout_solution.checkout('E') == 40
-    #
-    # def test_9(self):
-    #     assert checkout_solution.checkout('EE') == 80
-    #
-    # def test_10(self):
-    #     assert checkout_solution.checkout('EEBB') == 110
-    #
-    # def test_11(self):
-    #     assert checkout_solution.checkout('EEEE') == 160
-    #
-    # def test_12(self):
-    #     assert checkout_solution.checkout('AAAAA') == 200
-    #
-    # def test_13(self):
-    #     assert checkout_solution.checkout('AAAAAAAA') == 330
-    #
-    # def test_14(self):
-    #     assert checkout_solution.checkout('EEEEBB') == 160
-    #
-    # def test_15(self):
-    #     assert checkout_solution.checkout('FFF') == 20
-    #
-    # def test_16(self):
-    #     assert checkout_solution.checkout('FFFFFF') == 40
-    #
-    # def test_17(self):
-    #     assert checkout_solution.checkout('FFFF') == 30
-    #
-    # def test_18(self):
-    #     assert checkout_solution.checkout('FF') == 20
-    #
-    # def test_19(self):
-    #     assert checkout_solution.checkout('ABCDEF') == 165
-    #
-    # def test_20(self):
-    #     assert checkout_solution.checkout('RRRQ') == 150
-    #
-    # def test_21(self):
-    #     assert checkout_solution.checkout('UUUU') == 120
-    #
-    # def test_22(self):
-    #     assert checkout_solution.checkout('UUUUUU') == 200
-    #
-    # def test_23(self):
-    #     assert checkout_solution.checkout('UUU') == 120
+    def test_1(self):
+        assert checkout_solution.checkout('AAA') == 130
+
+    def test_2(self):
+        assert checkout_solution.checkout('BB') == 45
+
+    def test_3(self):
+        assert checkout_solution.checkout('AAABB') == 175
+
+    def test_4(self):
+        assert checkout_solution.checkout('AAAA') == 180
+
+    def test_5(self):
+        assert checkout_solution.checkout('BBB') == 75
+
+    def test_6(self):
+        assert checkout_solution.checkout('AAAAABBB') == 275
+
+    def test_7(self):
+        assert checkout_solution.checkout('ABCD') == 115
+
+    def test_8(self):
+        assert checkout_solution.checkout('E') == 40
+
+    def test_9(self):
+        assert checkout_solution.checkout('EE') == 80
+
+    def test_10(self):
+        assert checkout_solution.checkout('EEBB') == 110
+
+    def test_11(self):
+        assert checkout_solution.checkout('EEEE') == 160
+
+    def test_12(self):
+        assert checkout_solution.checkout('AAAAA') == 200
+
+    def test_13(self):
+        assert checkout_solution.checkout('AAAAAAAA') == 330
+
+    def test_14(self):
+        assert checkout_solution.checkout('EEEEBB') == 160
+
+    def test_15(self):
+        assert checkout_solution.checkout('FFF') == 20
+
+    def test_16(self):
+        assert checkout_solution.checkout('FFFFFF') == 40
+
+    def test_17(self):
+        assert checkout_solution.checkout('FFFF') == 30
+
+    def test_18(self):
+        assert checkout_solution.checkout('FF') == 20
+
+    def test_19(self):
+        assert checkout_solution.checkout('ABCDEF') == 165
+
+    def test_20(self):
+        assert checkout_solution.checkout('RRRQ') == 150
+
+    def test_21(self):
+        assert checkout_solution.checkout('UUUU') == 120
+
+    def test_22(self):
+        assert checkout_solution.checkout('UUUUUU') == 200
+
+    def test_23(self):
+        assert checkout_solution.checkout('UUU') == 120
 
     def test_24(self):
         assert checkout_solution.checkout('UUUUUUUU') == 240
+
 
