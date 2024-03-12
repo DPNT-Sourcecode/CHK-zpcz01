b9725f10ec694c53811a3896463954e6f15f65f2
sbx_user1051 <sbx_user1051@169.254.17.197>
2024-03-12 21:51:46 +0000

    Tue Mar 12 21:51:46 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -63,8 +63,8 @@ def calculate_item_free(prices, item_counts):
                 for key, value in free_item.items():
                     if key in item_counts:
                         sub_amount = free_count - prices[item].get('sub', 0)
-                        if sub_amount == 0:
-                            sub_amount = 1
+                        # if sub_amount == 0:
+                        #     sub_amount = 1
                         item_counts[key] -= value * sub_amount
                     else:
                         item_counts[key] = 0
@@ -80,3 +80,4 @@ def calculate_checkout_value(prices, item_counts):
             value += count * prices[item]['price']
 
     return value
+
